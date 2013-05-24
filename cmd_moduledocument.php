f83d28fd6478c5bf9c52f81ff3b095b0d955e290
Xing <xingxu@uw.edu>
2013-05-23 22:00:15 -0700

    add function celebrity_add; reporter_add

--- cmd_moduledocument.php
+++ cmd_moduledocument.php
@@ -37,7 +37,7 @@ function dispatchDocumentCmd($cmd, $cmd_list)
 		elseif ($arg1 == "update") {
 		$status = reporter_update($cmd_list);
 		}
-		elseif ($arg1 == "delete") {
+		elseif ($arg1 == "del") {
 			$status = reporter_del($cmd_list);
 		}
 		else {
@@ -47,23 +47,19 @@ function dispatchDocumentCmd($cmd, $cmd_list)
 
 	if ($cmd == "celebrity") { 
 
-		// $arg1 = "";
-		// if (count($cmd_list) > 1) {
-		// $arg1 = $cmd_list[1];
-		// }
+		$arg1 = "";
+		if (count($cmd_list) > 1) {
+		$arg1 = $cmd_list[1];
+		}
 	
-		// if ($arg1 == "create") {
-		// 	$status = wf_create($cmd_list);
-		// }
-		// elseif ($arg1 == "delete") {
-		// $status = wf_delete($cmd_list);
-		// }
-		// elseif ($arg1 == "list") {
-		// 	$status = wf_list($cmd_list);
-		// }
-		// else {
-		// 	$status = cCmdStatus_NOT_FOUND; 
-
+		if ($arg1 == "add") {
+			$status = celebrity_add($cmd_list);
+		}
+		elseif ($arg1 == "update") {
+		$status = celebrity_update($cmd_list);
+		}
+		else {
+			$status = cCmdStatus_NOT_FOUND; 
 		}
 
 	if ($cmd == "gossip") { 
@@ -113,6 +109,58 @@ function gossip_create($cmd_list) {
 
 
 	$sql = sprintf("select ggdb.create_gossip ('%s', '%s', '%s', '%s', '%s', '%s');", $wf, $sn, $r, $c, $t, $b);
+
+	$result = runScalarDbQuery($sql);
+
+	$t = "\n"; 
+	$gResult = $t . print_r($cmd_list,true);
+	return cCmdStatus_OK; 
+}
+
+/*
+ * Add reporter
+ * Author: Xing 5/23/13 9:25pm
+ */
+function reporter_add($cmd_list) {
+	global $gResult;
+
+	$id = getValue("-id",$cmd_list);
+	$f = getValue("-f",$cmd_list); 
+	$l = getValue("-l",$cmd_list); 
+	$c = getValue("-c",$cmd_list); 
+
+	if (($id == NULL) || ($f == NULL)|| ($l == NULL)|| ($c == NULL)) {
+		return cCmdStatus_ERROR; 
+	}
+
+
+	$sql = sprintf("select ggdb.add_reporter ('%s', '%s', '%s', '%f');", $id, $f, $l, $c);
+
+	$result = runScalarDbQuery($sql);
+
+	$t = "\n"; 
+	$gResult = $t . print_r($cmd_list,true);
+	return cCmdStatus_OK; 
+}
+
+/*
+ * Add celebrity
+ * Author: Xing 5/23/13 9:46pm
+ */
+function celebrity_add($cmd_list) {
+	global $gResult;
+
+	$f = getValue("-f",$cmd_list); 
+	$l = getValue("-l",$cmd_list); 
+	$n = getValue("-n",$cmd_list);
+	$b = getValue("-b",$cmd_list); 
+
+	if (($n == NULL) || ($f == NULL)|| ($l == NULL)|| ($b == NULL)) {
+		return cCmdStatus_ERROR; 
+	}
+
+
+	$sql = sprintf("select ggdb.add_celebrity ('%s', '%s', '%s', '%s');", $f, $l, $n, $b);
 
 	$result = runScalarDbQuery($sql);
 
